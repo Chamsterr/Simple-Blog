daa5141382568caf958792633b53e4fc5dd9a484
Chamster <89588942+Chamsterr@users.noreply.github.com>
2022-02-14 15:55:28 +0300

    2

---
+++ Blog/urls.py
@@ -0,0 +1,11 @@
+"""Определяет схемы URL для Blog"""
+
+from django.urls import path
+
+from . import views
+
+app_name = "Blog"
+urlpatterns = [
+    # Домашняя страница
+    path('', views.index, name='index'),
+]
--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """Домашняя страница приложения Blog"""
+    return render(request, 'Blog/index.html')
