1095849fe2c9797d1c84746ca41ac46ab557634b
Chamster <89588942+Chamsterr@users.noreply.github.com>
2022-02-21 23:57:35 +0300

    ))

--- Blog/forms.py
+++ Blog/forms.py
@@ -5,9 +5,13 @@ from .models import BlogPost
 class BlogForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ['title']
-        fields_2 = ['text']
+        fields = ('title', 'text')
         labels = {
                     'title': 'title',
                     'text': 'text'
                   }
+        widgets = {
+            'text': forms.Textarea(attrs={'class': 'text_f', 'placeholder': 'Write something...'}),
+            'title': forms.TextInput(attrs={'class': 'title_f', 'placeholder': 'Heading'}),
+        }
+
--- Blog/models.py
+++ Blog/models.py
@@ -9,5 +9,5 @@ class BlogPost(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.text[:750]}..."
+        return f"{self.text[:550]}..." if len(self.text[:]) > 550 else f"{self.text[:550]}"
 
